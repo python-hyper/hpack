1c232b30c7150bea69a044dc5070b6aa981b1c19
Cory Benfield <lukasaoz@gmail.com>
2015-10-12 13:12:50 +0100

    This project is now stable.

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     include_package_data=True,
     license='MIT License',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
