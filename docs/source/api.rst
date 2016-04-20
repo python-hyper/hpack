3bc823e4e01b7eb60dd741fa03012559744d80e0
Cory Benfield <lukasaoz@gmail.com>
2016-04-20 16:11:06 +0100

    Document HeaderTuple and export it.

--- docs/source/api.rst
+++ docs/source/api.rst
@@ -9,6 +9,12 @@ This document provides the HPACK API.
 .. autoclass:: hpack.Decoder
    :members: header_table_size, decode
 
+.. autoclass:: hpack.HeaderTuple
+   :members: indexable
+
+.. autoclass:: hpack.NeverIndexedHeaderTuple
+   :members: indexable
+
 .. autoclass:: hpack.HPACKError
 
 .. autoclass:: hpack.HPACKDecodingError
--- hpack/__init__.py
+++ hpack/__init__.py
@@ -6,11 +6,12 @@ hpack
 HTTP/2 header encoding for Python.
 """
 from .hpack import Encoder, Decoder
+from .struct import HeaderTuple, NeverIndexedHeaderTuple
 from .exceptions import HPACKError, HPACKDecodingError, InvalidTableIndex
 
 __all__ = [
     'Encoder', 'Decoder', 'HPACKError', 'HPACKDecodingError',
-    'InvalidTableIndex'
+    'InvalidTableIndex', 'HeaderTuple', 'NeverIndexedHeaderTuple'
 ]
 
 __version__ = '2.1.1'
