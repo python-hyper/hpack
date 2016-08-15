8dad504eeaf6c46be80cf6d7d057312b55d765eb
Cory Benfield <lukasaoz@gmail.com>
2016-08-15 11:18:25 +0100

    Try running coverage.

--- .travis/run.sh
+++ .travis/run.sh
@@ -6,6 +6,6 @@ set -x
 if [[ $TRAVIS_PYTHON_VERSION == pypy ]]; then
     py.test test/
 else
-    py.test --cov hpack test/
+    coverage run py.test test/
     coverage report -m --fail-under 100
 fi
