1c7cad40d26d3b89b5540221faa5e3b4fdef377f
Cory Benfield <lukasaoz@gmail.com>
2016-08-15 11:27:58 +0100

    Do better.

--- .travis/run.sh
+++ .travis/run.sh
@@ -6,6 +6,6 @@ set -x
 if [[ $TRAVIS_PYTHON_VERSION == pypy ]]; then
     py.test test/
 else
-    coverage run py.test test/
+    coverage run -m py.test test/
     coverage report -m --fail-under 100
 fi
