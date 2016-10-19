6080a9d868ac2484e9bfb8cd55571e56ee6a3915
Seth Michael Larson <sethmichaellarson@protonmail.com>
2016-10-19 09:21:58 -0500

    Add suggested changes

--- bench/test_hpack.py
+++ bench/test_hpack.py
@@ -1,33 +1,36 @@
-import hpack.hpack
+from hpack.hpack import (
+    encode_integer,
+    decode_integer
+)
 
 
 class TestHpackEncodingIntegersBenchmarks:
     def test_encode_small_integer_large_prefix(self, benchmark):
-        benchmark(hpack.hpack.encode_integer, 120, 7)
+        benchmark(encode_integer, integer=120, prefix_bits=7)
 
     def test_encode_small_integer_small_prefix(self, benchmark):
-        benchmark(hpack.hpack.encode_integer, 120, 1)
+        benchmark(encode_integer, integer=120, prefix_bits=1)
 
     def test_encode_large_integer_large_prefix(self, benchmark):
-        benchmark(hpack.hpack.encode_integer, 120000, 7)
+        benchmark(encode_integer, integer=120000, prefix_bits=7)
 
     def test_encode_large_integer_small_prefix(self, benchmark):
-        benchmark(hpack.hpack.encode_integer, 120000, 1)
+        benchmark(encode_integer, integer=120000, prefix_bits=1)
 
 
 class TestHpackDecodingIntegersBenchmarks:
     def test_decode_small_integer_large_prefix(self, benchmark):
-        data = hpack.hpack.encode_integer(120, 7)
-        benchmark(hpack.hpack.decode_integer, data, 7)
+        data = encode_integer(integer=120, prefix_bits=7)
+        benchmark(decode_integer, data=data, prefix_bits=7)
 
     def test_decode_small_integer_small_prefix(self, benchmark):
-        data = hpack.hpack.encode_integer(120, 1)
-        benchmark(hpack.hpack.decode_integer, data, 1)
+        data = encode_integer(integer=120, prefix_bits=1)
+        benchmark(decode_integer, data=data, prefix_bits=1)
 
     def test_decode_large_integer_large_prefix(self, benchmark):
-        data = hpack.hpack.encode_integer(120000, 7)
-        benchmark(hpack.hpack.decode_integer, data, 7)
+        data = encode_integer(integer=120000, prefix_bits=7)
+        benchmark(decode_integer, data=data, prefix_bits=7)
 
     def test_decode_large_integer_small_prefix(self, benchmark):
-        data = hpack.hpack.encode_integer(120000, 1)
-        benchmark(hpack.hpack.decode_integer, data, 1)
+        data = encode_integer(integer=120000, prefix_bits=1)
+        benchmark(decode_integer, data=data, prefix_bits=1)
