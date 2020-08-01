6299a8a3c2ee7fcdf781dcdd2a56757884efc779
Thomas Kriechbaumer <thomas@kriechbaumer.name>
2020-08-01 19:40:48 +0200

    remove py27-isms

--- src/hpack/__init__.py
+++ src/hpack/__init__.py
@@ -8,13 +8,23 @@ HTTP/2 header encoding for Python.
 from .hpack import Encoder, Decoder
 from .struct import HeaderTuple, NeverIndexedHeaderTuple
 from .exceptions import (
-    HPACKError, HPACKDecodingError, InvalidTableIndex, OversizedHeaderListError, InvalidTableSizeError
+    HPACKError,
+    HPACKDecodingError,
+    InvalidTableIndex,
+    OversizedHeaderListError,
+    InvalidTableSizeError
 )
 
 __all__ = [
-    'Encoder', 'Decoder', 'HPACKError', 'HPACKDecodingError',
-    'InvalidTableIndex', 'HeaderTuple', 'NeverIndexedHeaderTuple',
-    'OversizedHeaderListError', 'InvalidTableSizeError'
+    'Encoder',
+    'Decoder',
+    'HeaderTuple',
+    'NeverIndexedHeaderTuple',
+    'HPACKError',
+    'HPACKDecodingError',
+    'InvalidTableIndex',
+    'OversizedHeaderListError',
+    'InvalidTableSizeError',
 ]
 
 __version__ = '4.0.0+dev'
