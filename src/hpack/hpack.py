fb65910e49f98b137ea6ae5936980eafc67271cc
Burak Yigit Kaya <byk@sentry.io>
2024-11-16 16:49:36 +0000

    better naming and comment

--- src/hpack/hpack.py
+++ src/hpack/hpack.py
@@ -143,16 +143,16 @@ def _dict_to_iterable(header_dict):
         yield key, header_dict[key]
 
 
-def _to_bytes(string):
+def _to_bytes(value):
     """
-    Convert string to bytes.
+    Convert anything to bytes through a UTF-8 encoded string
     """
-    t = type(string)
+    t = type(value)
     if t is bytes:
-        return string
+        return value
     if t is not str:
-        string = str(string)
-    return string.encode("utf-8")
+        value = str(value)
+    return value.encode("utf-8")
 
 
 class Encoder:
